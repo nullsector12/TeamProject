b8073b7ef8234bfa024290adf91e78615f15e532
KimSeungYeon <rlamd0412@gmail.com>
2020-05-04 19:22:06 +0900

    메서드/변수 수정중

--- TextRPG/src/events/Events.java
+++ TextRPG/src/events/Events.java
@@ -2,19 +2,18 @@ package events;
 
 public abstract class Events implements EventsInterface{
 
-	int currentGold;
-	int currentExp;
-	int lossGold;
 	
+	boolean battleResult = true;
 	@Override
 	// 전투 결과에 따른 Gold와 Exp의 획득
 	public int playerGetRewords(int addGold, int addExp) {
 
 		if(battleResult == true) {
-			Player.currentGold += Monster.setgoldWorth;
-			Player.currentExp += Monster.setexpWorth;
-			return currentGold = addGold;
+			Player.setCurrentGold(Player.getCurrentGold() + Monster.setgoldWorth);
+			Player.setCurrentExp(Player.getCurrentExp() + Monster.setexpWorth);
+			return Player.getCurrentGold();
 			return currentExp = addExp; 
+			
 		}else {
 			// 패널티를 정해야함
 			playerGetPenalty();
@@ -24,7 +23,7 @@ public abstract class Events implements EventsInterface{
 	// 패배로 인한 골드의 감소
 	@Override
 	public void playerGetPenalty() {
-		currentGold = (Player.currentGold) - (100*Player.currentLevel);
+		currentGold = (Player.getCurrentGold()) - (100*Player.getCurrentLevel());
 		
 	}
 	
