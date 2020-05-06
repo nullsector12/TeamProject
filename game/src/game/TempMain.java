5e4c9d3a1af7d4a52b7bab67bba009bdbee0477e
yun <wyk0935@gmail.com>
2020-05-06 15:02:54 +0900

    battle dungeon

--- game/src/game/TempMain.java
+++ game/src/game/TempMain.java
@@ -3,35 +3,38 @@ package game;
 public class TempMain {
 
 	public static void main(String[] args) {
-		Monster m=new Monster();
+		Monster m = new Monster();
 //		Monster m=Monster.getInstance();
-		m=m.makeMonster(1);
-//		System.out.println(m);
-		System.out.println(m.getBaseHealth());
-		System.out.println(m.getBaseStrength());
-		System.out.println("----------------------------------");
-		m.showData();
-		m.encounterMonster();
-		
-		m=m.makeMonster(2);
-//		System.out.println(m);
-		System.out.println(m.getBaseHealth());
-		System.out.println(m.getBaseStrength());
-		System.out.println("----------------------------------");
-		m.showData();
-		m.encounterMonster();
-		
-		
-		Player p=new Player();
-		System.out.println(p);
-		
-		m=m.makeMonster(3);
-		m.showData();
-		m.encounterMonster();
-		m.attack(p, 20);
-		System.out.println("----------------------------------");
-		System.out.println(p);
-		
+//		m = m.makeMonster(1);
+////		System.out.println(m);
+//		System.out.println(m.getBaseHealth());
+//		System.out.println(m.getBaseStrength());
+//		System.out.println("----------------------------------");
+//		m.showData();
+//		m.encounterMonster();
+//
+//		m = m.makeMonster(2);
+////		System.out.println(m);
+//		System.out.println(m.getBaseHealth());
+//		System.out.println(m.getBaseStrength());
+//		System.out.println("----------------------------------");
+//		m.showData();
+//		m.encounterMonster();
+//
+		Player p = new Player();
+//		System.out.println(p);
+//
+//		m = m.makeMonster(3);
+//		m.showData();
+//		m.encounterMonster();
+//		m.attack(p, 20);
+//		System.out.println("----------------------------------");
+//		System.out.println(p);
+
+		Battle b = new Battle();
+		p.addName();
+		System.out.println(p.getName());
+		b.choicePlayerMovement(m,p);
 	}
 
 }
